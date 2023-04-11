e684be055994220a450273f2156511a46e3efcbb
Juancams <jc.manzanares.serrano@gmail.com>
2023-04-11 17:01:36 +0200

    Initial commit

---
+++ dialogflow_ros2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'dialogflow_ros2'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Juan Carlos Manzanares Serrano',
+    maintainer_email='jc.manzanares.serrano@gmail.com',
+    description='Dialogflow ros2',
+    license='Apache License, Version 2.0',
+    tests_require=[''],
+    entry_points={
+        'console_scripts': [
+            'dialogflow_client = dialogflow_ros2.dialogflow_client:main'
+        ],
+    },
+)
